3816d5d26a7a0b2e6b2f6fdd33efa1a333035f48
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2022-12-20 21:39:09 +0000

    CTCP-1578: upgrade to Scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,16 @@ import sbt._
 
 object AppDependencies {
 
-  val AkkaVersion = "2.6.19"
-  val catsVersion = "2.7.0"
-  val dispatchV = "1.2.0"
-  val bootstrapVersion = "6.4.0"
+  val AkkaVersion      = "2.6.19"
+  val catsVersion      = "2.7.0"
+  val dispatchV        = "1.2.0"
+  val bootstrapVersion = "7.12.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "com.lightbend.akka"     %% "akka-stream-alpakka-xml"   % "3.0.4",
-    "com.typesafe.akka"      %% "akka-stream"               % AkkaVersion,
-    "org.typelevel"          %% "cats-core"                 % catsVersion,
-
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVersion,
+    "com.lightbend.akka" %% "akka-stream-alpakka-xml"   % "3.0.4",
+    "com.typesafe.akka"  %% "akka-stream"               % AkkaVersion,
+    "org.typelevel"      %% "cats-core"                 % catsVersion,
     // required for scalaxb
     "org.scala-lang.modules" %% "scala-xml"                % "1.3.0",
     "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
