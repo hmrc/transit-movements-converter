6b5b61edb5fef51e30c488143645a5cf4b8c6458
DerekPalmer <35488891+derekspalmer@users.noreply.github.com>
2024-01-15 09:15:01 +0000

    Ctcp 4587 play3 upgrade (#19)
    * CTCP-4587: Play 3.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,26 +6,22 @@ import sbt._
 
 object AppDependencies {
 
-  val AkkaVersion      = "2.6.19"
-  val catsVersion      = "2.7.0"
-  val bootstrapVersion = "7.12.0"
+  val catsVersion      = "2.9.0"
+  val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"        %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "com.lightbend.akka" %% "akka-stream-alpakka-xml"   % "3.0.4",
-    "com.typesafe.akka"  %% "akka-stream"               % AkkaVersion,
-    "org.typelevel"      %% "cats-core"                 % catsVersion,
+    "uk.gov.hmrc"      %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "org.apache.pekko" %% "pekko-connectors-xml"      % "1.0.1",
+    "org.apache.pekko" %% "pekko-stream"              % "1.0.1",
+    "org.typelevel"    %% "cats-core"                 % catsVersion,
     // required for scalaxb
-    "org.scala-lang.modules" %% "scala-xml"                % "1.3.0",
     "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2",
     "javax.xml.bind"          % "jaxb-api"                 % "2.3.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootstrapVersion,
-    "com.typesafe.play"   %% "play-test"              % current,
-    "com.vladsch.flexmark" % "flexmark-all"           % "0.36.8",
-    "org.scalatestplus"   %% "scalacheck-1-15"        % "3.2.2.0",
-    "org.scalatestplus"   %% "mockito-3-2"            % "3.1.2.0"
-  ).map(_ % "test, it")
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.scalatestplus" %% "scalacheck-1-15"        % "3.2.2.0",
+    "org.scalatestplus" %% "mockito-3-2"            % "3.1.2.0"
+  ).map(_ % Test)
 }
